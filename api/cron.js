16a3a8da6c56e9a105e5f8137fe90319e79d8f09
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-11-18 16:00:19 +0600

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -5,16 +5,13 @@ export default async function handler(req, res) {
   try {
     const text = await composeMessage(false);
 
-    await axios.post(
-      `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
-      {
-        chat_id: process.env.CHAT_ID,
-        text
-      }
-    );
+    await axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`, {
+      chat_id: process.env.CHAT_ID,
+      text
+    });
 
-    res.status(200).json({ ok: true });
-  } catch (e) {
-    res.status(200).json({ error: e.message });
+    return res.status(200).json({ ok: true });
+  } catch (err) {
+    return res.status(200).json({ error: err.message });
   }
 }
